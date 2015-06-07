e429abfd02198f4e2ff8d64309db3b5178f74001
Justin Murdock <jmurdock@lyris.com>
2015-06-07 14:46:59 -0700

    finish rest of keys

--- app.js
+++ app.js
@@ -67,6 +67,11 @@ angular.module('Keyboard',[])
             key: '=',
             code: 187,
             classes: 'single'
+        },
+        {
+            key: 'bck',
+            code: 8,
+            classes: 'bck'
         }
     ];
     $scope.secondRow = [{
@@ -85,9 +90,216 @@ angular.module('Keyboard',[])
             key: 'e',
             code: 69,
             classes: 'single'
+        },{
+            key: 'r',
+            code: 82,
+            classes: 'single'
+        },{
+            key: 't',
+            code: 84,
+            classes: 'single'
+        },{
+            key: 'y',
+            code: 89,
+            classes: 'single'
+        },{
+            key: 'u',
+            code: 85,
+            classes: 'single'
+        },{
+            key: 'i',
+            code: 73,
+            classes: 'single'
+        },{
+            key: 'o',
+            code: 79,
+            classes: 'single'
+            
+        },{
+            key: 'p',
+            code: 80,
+            classes: 'single'
+        },{
+            key: '[',
+            code: 219,
+            classes: 'single'
+        },{
+            key: ']',
+            code: 221,
+            classes: 'single'
+        },{
+            key: '\\',
+            code: 220,
+            classes: 'single'
         }
     ];
-    $scope.rows = [$scope.firstRow, $scope.secondRow];
+    $scope.thirdRow = [
+        {
+            key: 'caps',
+            code: 20,
+            classes: 'caps'
+        },
+        {
+            key: 'a',
+            code: 65,
+            classes: 'single'
+        },
+        {
+            key: 's',
+            code: 83,
+            classes: 'single'
+        },
+        {
+            key: 'd',
+            code: 68,
+            classes: 'single'
+        },
+        {
+            key: 'f',
+            code: 70,
+            classes: 'single'
+        },
+        {
+            key: 'g',
+            code: 71,
+            classes: 'single'
+        },
+        {
+            key: 'h',
+            code: 72,
+            classes: 'single'
+        },
+        {
+            key: 'j',
+            code: 74,
+            classes: 'single'
+        },
+        {
+            key: 'k',
+            code: 75,
+            classes: 'single'
+        },
+        {
+            key: 'l',
+            code: 76,
+            classes: 'single'
+        },
+        {
+            key: ';',
+            code: 186,
+            classes: 'single'
+        },
+        {
+            key: '\'',
+            code: 222,
+            classes: 'single'
+        },
+        {
+            key: 'enter',
+            code: 13,
+            classes: 'enter'
+        }
+    ];
+    $scope.fourthRow = [
+        {
+            key: 'shift',
+            code: 16,
+            classes: 'shift'
+        },
+        {
+            key: 'z',
+            code: 90,
+            classes: 'single'
+        },
+        {
+            key: 'x',
+            code: 88,
+            classes: 'single'
+        },
+        {
+            key: 'c',
+            code: 67,
+            classes: 'single'
+        },
+        {
+            key: 'v',
+            code: 86,
+            classes: 'single'
+        },
+        {
+            key: 'b',
+            code: 66,
+            classes: 'single'
+        },
+        {
+            key: 'n',
+            code: 77,
+            classes: 'single'
+        },
+        {
+            key: 'm',
+            code: 78,
+            classes: 'single'
+        },
+        {
+            key: ',',
+            code: 188,
+            classes: 'single'
+        },
+        {
+            key: '.',
+            code: 190,
+            classes: 'single'
+        },
+        {
+            key: '/',
+            code: 191,
+            classes: 'single'
+        },
+        {
+            key: 'shift',
+            code: 16,
+            classes: 'shift'
+        }
+    ];
+    $scope.fifthRow = [
+        {
+            key: 'ctrl',
+            code: 17,
+            classes: 'ctrl'
+        },
+        {
+            key: 'cmd',
+            code: 91,
+            classes: 'cmd'
+        },
+        {
+            key: 'alt',
+            code: 18,
+            classes: 'alt'
+        },
+        {
+            key: ' ',
+            code: 32,
+            classes: 'spacebar'
+        },
+        {
+            key: 'alt',
+            code: 18,
+            classes: 'alt'
+        },
+        {
+            key: 'cmd',
+            code: 91,
+            classes: 'cmd'
+        },
+        {
+            key: 'ctrl',
+            code: 17,
+            classes: 'ctrl'
+        }
+    ];
+    $scope.rows = [$scope.firstRow, $scope.secondRow, $scope.thirdRow, $scope.fourthRow, $scope.fifthRow];
 })
 .directive('keyboard', ['$document', '$timeout', function($document, $timeout) {
     return {
